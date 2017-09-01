8a7c71c0c2464781b9c7f1c2b23383b94826dde0
TahitiP4ncake <dorian.pancake@gmail.com>
2017-09-01 18:11:22 +0200

    x input + shaderforge

---
+++ Hyper/Assets/Scripts/letest.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using XInputDotNetPure;
+using UnityEngine;
+
+public class letest : MonoBehaviour {
+    public x360_Gamepad gamepad;
+
+    
+
+    private GamepadManager manager;
+	// Use this for initialization
+	void Start () {
+        manager = GamepadManager.Instance;
+        //  gamepad = manager.GetGamepad();
+        gamepad=manager.GetGamepad(1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (gamepad.GetButtonDown("A"))
+        {
+            Debug.Log("oui");
+        }
+    }
+   
+    }
+
