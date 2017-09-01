42bf4a94eba92491e2d3ad776bed43d5f3fb764a
Baptiste <b.billet@rubika-edu.com>
2017-09-02 00:50:54 +0200

    Player

---
+++ Hyper/Assets/Scripts/Player.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using FluffyUnderware.Curvy.Controllers;
+using FluffyUnderware.Curvy;
+
+public class Player : MonoBehaviour {
+
+	public Rigidbody rigid;
+
+	public SplineController controller;
+
+	private int splineNumber;
+
+	public int playerIndex;
+
+	public float speed=0;
+
+	private float naturalSpeed = 5;
+
+	private float splineSpeed;
+
+	private float boosterSpeed = 1;
+
+	private float timeSpeed = 1;
+
+	private float calculSpeed;
+
+	/*
+	public void SetUpPlayer(CurvySpline _spline, int _number)
+	{ 
+
+		controller.SwitchTo(_spline, controller.RelativePosition, 0f);
+
+		splineNumber = _number;
+
+		playerIndex = _number; 
+	}*/
+
+	public void StartRace()
+	{
+
+		splineNumber = playerIndex;
+
+		SpeedCalcul();
+
+
+	}
+
+	public void ChangeSpline(bool _inner)
+	{
+		if(_inner)
+		{
+			if(splineNumber ==0)
+			{
+				return;
+			}
+
+			splineNumber--;
+
+			controller.SwitchTo(GameManager.instance.splines[splineNumber], controller.RelativePosition, 0.1f);
+
+		}
+		else
+		{
+			if ( splineNumber == 3 )
+			{
+				return;
+			}
+
+			splineNumber++;
+
+			controller.SwitchTo(GameManager.instance.splines [splineNumber], controller.RelativePosition, 0.1f);
+		}
+
+
+
+	}
+
+	void SpeedCalcul()
+	{
+		splineSpeed = 1.0f + ( (3.0f - splineNumber)/100.0f);
+
+		Debug.Log(splineSpeed);
+
+		calculSpeed = naturalSpeed * splineSpeed * timeSpeed;
+
+		calculSpeed *= boosterSpeed;
+
+		speed = calculSpeed;
+
+		//controller.Speed = speed;
+	}
+
+	// Update is called once per frame
+	void Update () 
+	{
+		controller.Speed = Mathf.Lerp(controller.Speed, speed, Time.deltaTime);
+
+		if ( rigid != null )
+		{
+			Debug.Log(rigid.velocity.magnitude);
+		}
+	}
+}
---
+++ Hyper/Assets/Scripts/TerrainLine.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TerrainLine : MonoBehaviour {
+
+	LineRenderer lineRenderer;
+
+	// Use this for initialization
+	void Start () 
+	{
+		lineRenderer = GetComponent<LineRenderer>();
+
+		Vector3[] _linePositions = new Vector3 [lineRenderer.transform.childCount];
+	
+		for(int i = 0; i< lineRenderer.transform.childCount; i++)
+		{
+			_linePositions [i] = lineRenderer.transform.GetChild(i).transform.position;
+		}
+
+		lineRenderer.positionCount = _linePositions.Length;
+
+		lineRenderer.SetPositions(_linePositions);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
