ecefaf7fd52c3cf2a753acb2d02087b9d98d4754
Baptiste <b.billet@rubika-edu.com>
2017-09-04 23:32:08 +0200

    directions

---
+++ Assets/Scripts/ChangeDirection.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Controls
+{
+	Left,
+	Right,
+	Up,
+	Down
+}
+
+public class ChangeDirection : MonoBehaviour {
+
+	public Controls goInner;
+	public Controls goOuter;	
+
+
+}
