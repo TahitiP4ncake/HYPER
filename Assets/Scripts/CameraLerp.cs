2edc37d1f5c7bdf03847d443a7df34160a2bf00e
TahitiP4ncake <dorian.pancake@gmail.com>
2017-09-05 20:37:51 +0200

    camera lerp qui marche

--- Assets/Scripts/CameraLerp.cs
+++ Assets/Scripts/CameraLerp.cs
@@ -8,26 +8,37 @@ public class CameraLerp : MonoBehaviour {
     public AnimationCurve vertical;
     public AnimationCurve forward;
 
-    public float speed=1;
+    public float speed = 1;
     Vector3 origin;
 
+    float localTime;
 	// Use this for initialization
 	void Start () {
         origin = transform.eulerAngles;
+        StartCoroutine(CameraTime());
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
         transform.eulerAngles = new Vector3(
-            origin.x + horizontal.Evaluate(Time.time*speed),
-            origin.y + vertical.Evaluate(Time.time*speed),
-            origin.z + forward.Evaluate(Time.time*speed));
+            origin.x + horizontal.Evaluate(localTime),
+            origin.y + vertical.Evaluate(localTime),
+            origin.z + forward.Evaluate(localTime));
         /*
         if(Input.GetKeyDown(KeyCode.Space))
         {
             speed += 0.1f;
         }
         */
+    }
+
+    IEnumerator CameraTime()
+    {
+        while(true)
+        {
+            localTime += Time.deltaTime * speed;
+            yield return new WaitForEndOfFrame();
+        }
     }
 }
